4ceceeb399e29c5a47add674c7be3eb4ffc74d46
Mahmoud Easa <mu01011422865@gmail.com>
2022-11-24 10:08:39 +0200

    Initial Commit

---
+++ admin.php
@@ -0,0 +1,58 @@
+<?php
+
+    include "header.php";
+
+?>
+
+<table>
+    <th>الرقم</th>
+    <th>إسم المريض</th>
+    <th>البريد الإلكتروني</th>
+    <th>التاريخ</th>
+    <th></th>
+
+<?php
+
+    $host               = "localhost";
+    $user               = "root";
+    $password           = "";
+    $dbName             = "hospital";
+
+    $conn = mysqli_connect($host, $user, $password,$dbName);
+
+    // إستيراد معلومات المرضى من قاعدة البيانات
+
+    $query = "SELECT * FROM patients";
+    $result = mysqli_query($conn,$query);
+    
+    if(isset($_GET["delete"])){
+        $queryDelete = "delete from patients where id='{$_GET['id']}'";
+        $deleted = mysqli_query($conn, $queryDelete);
+
+        if($deleted) {
+            header("Location: admin.php");
+        }else {
+            echo "<p style='color:red'>" . "عفواً حدث خطأ ما .. حاول مجدد " . "</p>";
+        }
+    }
+
+    if ($result){
+        while($row = mysqli_fetch_assoc($result)){
+            echo "<tr>
+                    <td>" . $row['id'] . "</td>
+                    <td>" . $row['name'] . "</td>
+                    <td>" . $row['email'] . "</td>
+                    <td>" . $row['date'] . "</td>
+                    <td>
+                        <a href='admin.php?delete=true&id={$row['id']}'>Delete</a>
+                    </td>
+                </tr>";
+        }
+        echo "</table>";
+    }
+    else{
+        echo "يوجد خطا ما";
+    }
+
+
+?>
